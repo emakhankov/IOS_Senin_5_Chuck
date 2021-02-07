0fdd7fb7162e82ff6f52c0704ddbffad06a2b6f4
Evgeny Makhankov <EMakhankov@gmail.com>
2021-02-07 17:42:45 +0300

    Initial Commit

---
+++ IOS_Senin_5_Chuck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_Senin_5_Chuck
+//
+//  Created by Jenya on 07.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
